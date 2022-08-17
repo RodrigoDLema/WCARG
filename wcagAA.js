e79f1b7411958b1e2cfdc9ef01c2983972c7e64d
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-08-17 01:03:31 -0300

    Update wcagAA.js

--- wcagAA.js
+++ wcagAA.js
@@ -1,11 +1,12 @@
 'use strict';
 
-
 const pa11y = require('pa11y');
 
 function runPally() {
     pa11y('https://www.eventbrite.com/', {
         standard: 'WCAG2AA',
+        includeNotices: true,
+        includeWarnings: true,
         log: {
             debug: console.log,
             error: console.error,
