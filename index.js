29a00c62c6f12dfb2ee3cb35b0d8ecf0a5fd32d2
Yosoyquemero <54010778+Yosoyquemero@users.noreply.github.com>
2022-08-16 22:01:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ async function run() {
 
 let seleccionados = [];
 
-let wcag = ["WCAG2AA.Principle4.Guideline4_1.4_1_1","WCAG2AA.Principle1.Guideline1_3.1_3_1","WCAG2AA.Principle4.Guideline4_1.4_1_2"];
+let wcag = ["WCAG2AA.Principle4.Guideline4_1.4_1_1", "WCAG2AA.Principle4.Guideline4_1.4_1_2", "WCAG2AA.Principle1.Guideline1_3.1_3_1", "WCAG2AA.Principle1.Guideline1_4.1_4_3"];
+
+//console.log(results.issues)
 
 results.issues.forEach(element => {
 
